52f96955f641a594bb8d71deebb40a44ec3086ef
seba <seba81281@gmail.com>
2016-08-30 15:28:01 -0300

    se crean archivos de la version 1.1 del proyecto cliente y del servidor

---
+++ Cliente_v1.1/Client.cpp
@@ -0,0 +1,144 @@
+#include "Client.h"
+
+
+Client::Client(int af, int tipo, int protocolo, std::string ip, std::string puerto,const char* usuario, std::string clave)
+{
+	this->ClientConnectionSocket = INVALID_SOCKET;
+
+
+	this->result = NULL;
+	this->puerto = puerto;
+	this->ipServer = ip;
+	this->ptr = NULL;
+	this->usuario = usuario;
+	this->clave = clave;
+
+	
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+
+		std::cout << "Error al inicializar el socket del cliente: " << WSAGetLastError() << std::endl;
+		system("pause");
+		WSACleanup();
+		exit(0);
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = af;
+	hints.ai_socktype = tipo;
+	hints.ai_protocol = protocolo;
+
+	// configuracion de ip y puerto de conexion con el server
+
+	if (getaddrinfo(this->ipServer.c_str(), this->puerto.c_str(), &(this->hints), &(this->result)) != NO_ERROR) {
+		
+		std::cout << "Error al inicializar configuracion de ip y puerto" << std::endl;
+		system("pause");
+		WSACleanup();
+		exit(0);
+	}
+
+	//**************************//
+	this->ptr = this->result; 
+	//creacion de socket para conectarse con el servidor
+	this->ClientConnectionSocket = socket(ptr->ai_family, ptr->ai_socktype,ptr->ai_protocol);
+	if (this->ClientConnectionSocket == INVALID_SOCKET) 
+	{
+		std::cout << "Error al crear el socket del cliente: " << WSAGetLastError() << std::endl;
+		system("pause");
+		freeaddrinfo(result);
+		WSACleanup();
+		exit(0);
+	}
+
+}
+
+void Client::conectarAServidor()
+{
+	// conectarse con la ip del server
+	for (this->ptr= this->result; this->ptr != NULL; this->ptr = this->ptr->ai_next) {
+
+
+		// Conectando al servidor
+
+		if (connect(this->ClientConnectionSocket, this->ptr->ai_addr, (int)this->ptr->ai_addrlen) == SOCKET_ERROR)
+		{
+			closesocket(this->ClientConnectionSocket);
+			this->ClientConnectionSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(this->result);
+
+	if (this->ClientConnectionSocket == INVALID_SOCKET) {
+		std::cout << "fallo al intentar conectar con el servidor" << WSAGetLastError() << std::endl;
+		system("pause");
+		WSACleanup();
+		freeaddrinfo(this->result);
+		exit(0);
+
+
+	}
+
+
+}
+
+
+int Client::enviarDatos(const char* datosEnviados, int sizeDatos)
+{
+	int cantidadBytesEnviados;
+	cantidadBytesEnviados = send(this->ClientConnectionSocket, datosEnviados, sizeDatos, 0);
+	if (cantidadBytesEnviados == SOCKET_ERROR) {
+
+		std::cout << "fallo al enviar los datos: " << WSAGetLastError() << std::endl;
+		system("pause");
+		exit(0);
+	}
+
+	std::cout << "Bytes enviados al Server: " << cantidadBytesEnviados << std::endl;
+	return cantidadBytesEnviados;
+}
+
+int Client::recibirDatos(char* datosRecibidos, int sizeDatos)
+{
+	int cantidadBytesRecibidos;
+
+	
+	cantidadBytesRecibidos = recv(this->ClientConnectionSocket, datosRecibidos, sizeDatos, 0);
+	if (cantidadBytesRecibidos > 0) {
+		std::cout << "Bytes recibidos: " << cantidadBytesRecibidos << std::endl;
+		datosRecibidos[cantidadBytesRecibidos] = 0;
+
+	}
+	else
+		if(cantidadBytesRecibidos == 0)
+		std::cout << "Bytes recibidos: " << cantidadBytesRecibidos << " la conexion esta cerrada" <<  std::endl;
+		else
+			std::cout << "fallo con error: " <<  WSAGetLastError();
+
+	return cantidadBytesRecibidos;
+
+}
+const char* Client::getUserClientName()
+{
+	return this->usuario;
+
+}
+void Client::cerrarConexionConServer()
+{
+
+	// shutdown the connection since no more data will be sent
+
+	if (shutdown(this->ClientConnectionSocket, SD_SEND) == SOCKET_ERROR) {
+		std::cout << "fallo al cerrar la conexion con el server: " << WSAGetLastError();
+		closesocket(this->ClientConnectionSocket);
+		WSACleanup();
+		exit(0);
+	}
+
+}
+Client::~Client()
+{
+	closesocket(this->ClientConnectionSocket);
+}
---
+++ Servidor_v1.1/Server.h
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef _Server_h_
+#define _Server_h_
+
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+
+class Server
+{
+
+
+
+
+private:
+	WSADATA wsaData;
+	SOCKET serverListenSocket;
+	SOCKET backupListenSocket;
+	SOCKET acceptSocket;
+	sockaddr_in dirSocketServidor;
+	struct addrinfo *result;
+	struct addrinfo hints;
+
+
+	int iSendResult;
+
+
+	std::string puerto;
+
+public:
+	Server();
+
+	/*af=AF_INET  The Internet Protocol version 4 (IPv4) address family.
+	tipo=SOCK_STREAM  A socket type that provides sequenced, reliable, two-way, connection-based byte streams
+	protocolo=IPPROTO_TCP*/
+	Server(int af, int tipo, int protocolo,std::string puerto);
+
+	void binding(int af);
+	void escucharConexiones();
+	void aceptarConexion();
+	int enviarDatos(const char* datosEnviados, int sizeDatos);
+	int recibirDatos(char* datosRecibidos, int sizeDatos);
+	bool aceptaConexion();
+	void cerrarConexion();
+	bool isSocketInvalido();
+	~Server();
+};
+
+#endif
