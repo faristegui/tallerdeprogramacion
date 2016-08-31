2e82835e26697fc36f8fab7c364a94348e6a812a
faristegui <faristegui@fi.uba.ar>
2016-08-30 22:18:18 -0300

    Cambios en TestCliente - Menu

--- Cliente/TestClient.cpp
+++ Cliente/TestClient.cpp
@@ -16,6 +16,12 @@ using namespace std;
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
 
+#ifdef WIN32 
+#define clear() system("cls"); 
+#else 
+#define clear() system("clear"); 
+#endif
+
 #define bufferSize 512
 int opcion;
 
@@ -77,51 +83,69 @@ void ThreadPrincipal(void* pParams)
 	}
 }
 */
-int main(int argc, char **argv)
+
+void menuPrincipal()
 {
 	string puerto;
 	string ip;
 	string user;
 	string clave;
+
+	opcion = 0; //Opción por defecto
+
+	while((opcion < 1) || (opcion > 6))
+	{
+		clear(); //Limpia la pantalla
+		cout << "MENU PRINCIPAL" << endl << endl <<
+		"1- Conectar" << endl <<
+		"2- Desconectar" << endl <<
+		"3- Salir" << endl <<
+		"4- Enviar" << endl <<
+		"5- Recibir" << endl <<
+		"6- Lore Ipsum" << endl << endl << "Ingrese una opcion: ";
+		cin >> opcion;
+	}
+	switch(opcion)
+	{
+		case 1:
+			{
+				solicitarDatos(puerto,ip,user,clave);
+				Client cliente(AF_INET, SOCK_STREAM, IPPROTO_TCP, ip, puerto, user.c_str(), clave);
+				cliente.conectarAServidor();
+				//falta completar, seria lo de abajo que esta comentado
+			}
+			//cliente.conectar()
+			break;
+		case 2:
+			//cliente.desconectar()
+			break;
+		case 3:
+			exit(0);
+			break;
+		case 4:
+			//cliente.enviar()
+			break;
+		case 5:
+			//cliente.recibir()
+			break;
+		case 6:
+			//cliente.enviarCiclicamente()
+			break;
+	}
+}
+
+int main(int argc, char **argv)
+{
 	while(true)
 	{
-		cout << "Ingrese una opción entre 1 y 6" << endl <<
-		"1- Conectar" << endl <<
-		"2-Desconectar" << endl <<
-		"3-Salir" << endl <<
-		"4-Enviar" << endl <<
-		"5-Recibir" << endl <<
-		"6-Lore Ipsum" << endl;
-		cin >> opcion;
-		switch(opcion)
-		{
-			case 1:
-				{
-					solicitarDatos(puerto,ip,user,clave);
-					Client cliente(AF_INET, SOCK_STREAM, IPPROTO_TCP, ip, puerto, user.c_str(), clave);
-					cliente.conectarAServidor();
-					//falta completar, seria lo de abajo que esta comentado
-				}
-				//cliente.conectar()
-				break;
-			case 2:
-				//cliente.desconectar()
-				break;
-			case 3:
-				exit(0);
-				break;
-			case 4:
-				//cliente.enviar()
-				break;
-			case 5:
-				//cliente.recibir()
-				break;
-			case 6:
-				//cliente.enviarCiclicamente()
-				break;
-		}
+		menuPrincipal();
 	}
-	//system("pause");	
+
+	
+	return 0;
+}
+
+//system("pause");	
 	/*
 	int respuestaServer;
 	char bufferDeRespuestaServer[bufferSize];
@@ -154,5 +178,3 @@ int main(int argc, char **argv)
 
 	cliente.cerrarConexionConServer();
 	*/
-	return 0;
-}
