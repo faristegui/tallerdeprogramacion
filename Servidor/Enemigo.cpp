03773bd5a5e571eead54205bdf4f403fbd5ac9ce
rayoazul <rayoazul@192.168.1.8>
2016-11-23 11:55:18 -0300

    Agrego avion segundo enemigo final.

--- Servidor/Enemigo.cpp
+++ Servidor/Enemigo.cpp
@@ -163,6 +163,26 @@ void Enemigo::mover()
 			}
 		}
 	}
+	if (Nombre == "EnemigoFinal2") 
+	{
+		if(this->estado == "CAMINA-IZQ")
+		{
+			x-=velocidad;
+		}
+		if (x < 10)
+		{
+			this->estado = "CAMINA-DER";
+		}
+		if(this->estado=="CAMINA-DER")
+		{
+			x+=velocidad;
+			if(x >= 500)
+			{
+				this->estado="CAMINA-IZQ";
+			}
+		}
+
+	}
 
 	if (Nombre == "HumanoEnemigo") {
 
